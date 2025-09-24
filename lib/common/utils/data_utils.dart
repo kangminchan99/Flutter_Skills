b3a96a3bac985efb394128acdb644fca9eabb4ed
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-09-24 12:47:48 +0900

    RatingModel

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -2,4 +2,8 @@ import 'package:flutterskills/common/const/data.dart';
 
 class DataUtils {
   static String pathToUrl(String path) => 'http://$ip$path';
+
+  static List<String> listPathToUrl(List<String> paths) {
+    return paths.map((e) => pathToUrl(e)).toList();
+  }
 }
