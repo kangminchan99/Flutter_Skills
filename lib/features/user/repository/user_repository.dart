4345e5a4de1e489425838040270ca72cb1b89e6f
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-09-25 21:28:11 +0900

    User Repository

---
+++ lib/features/user/repository/user_repository.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterskills/common/const/data.dart';
+import 'package:flutterskills/common/dio/dio_interceptor.dart';
+import 'package:flutterskills/features/user/model/user_model.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+
+part 'user_repository.g.dart';
+
+// http://$ip/user/me
+@RestApi()
+abstract class UserRepository {
+  factory UserRepository(Dio dio, {String baseUrl}) = _UserRepository;
+
+  @GET('/')
+  @Headers({'accessToken': 'true'})
+  Future<UserModel> getMe();
+}
+
+final userRepositoryProvider = Provider<UserRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return UserRepository(dio, baseUrl: 'http://$ip/user/me');
+});
