05bfafe99788ad06c6bd9b7240423456c703d6c7
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-09-25 22:45:03 +0900

    Login & Token Refresh Repository

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutterskills/common/const/data.dart';
 
 class DataUtils {
@@ -5,5 +7,14 @@ class DataUtils {
 
   static List<String> listPathToUrl(List paths) {
     return paths.map((e) => pathToUrl(e)).toList();
+  }
+
+  static String plainToBase64(String plain) {
+    // String -> base64
+    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+    // 인코딩
+    String encoded = stringToBase64.encode(plain);
+
+    return encoded;
   }
 }
---
+++ lib/features/user/repository/auth_repository.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:flutterskills/common/model/login_response.dart';
+import 'package:flutterskills/common/model/token_response.dart';
+import 'package:flutterskills/common/utils/data_utils.dart';
+
+class AuthRepository {
+  // http://$ip/auth
+  final String baseUrl;
+  final Dio dio;
+  AuthRepository({required this.baseUrl, required this.dio});
+
+  Future<LoginResponse> login({
+    required String username,
+    required String password,
+  }) async {
+    final serialized = DataUtils.plainToBase64('$username:$password');
+
+    final response = await dio.post(
+      '$baseUrl/login',
+      options: Options(headers: {'authorization': 'Basic $serialized'}),
+    );
+
+    return LoginResponse.fromJson(response.data);
+  }
+
+  Future<TokenResponse> getToken() async {
+    final response = await dio.post(
+      '$baseUrl/token',
+      options: Options(headers: {'refreshToken': 'true'}),
+    );
+
+    return TokenResponse.fromJson(response.data);
+  }
+}
