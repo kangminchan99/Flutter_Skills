3587eb35e62b20af9e99ca749f53e3cf776bfee9
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-09-26 18:17:42 +0900

    OrderRepository

---
+++ lib/features/order/repository/order_repository.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterskills/common/const/data.dart';
+import 'package:flutterskills/common/dio/dio_interceptor.dart';
+import 'package:flutterskills/features/order/model/order_model.dart';
+import 'package:flutterskills/features/order/model/post_order_body.dart';
+import 'package:retrofit/http.dart';
+
+part 'order_repository.g.dart';
+
+final orderRepositoryProvider = Provider<OrderRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+  return OrderRepository(dio, baseUrl: 'http://$ip/order');
+});
+
+// http://$ip/order
+@RestApi()
+abstract class OrderRepository {
+  factory OrderRepository(Dio dio, {String baseUrl}) = _OrderRepository;
+
+  @POST('')
+  @Headers({'accessToken': 'true'})
+  Future<OrderModel> postOrder({@Body() required PostOrderBody body});
+}
