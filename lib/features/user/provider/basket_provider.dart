c033bf69bb9d320f752add3a3027fa3d104147b4
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-09-26 15:24:25 +0900

    BasketProvider

---
+++ lib/features/user/provider/basket_provider.dart
@@ -0,0 +1,54 @@
+import 'package:collection/collection.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterskills/features/product/model/product_model.dart';
+import 'package:flutterskills/features/user/model/basket_item_model.dart';
+
+class BasketProvider extends StateNotifier<List<BasketItemModel>> {
+  BasketProvider() : super([]);
+
+  Future<void> addToBasket({required ProductModel product}) async {
+    // 1) 아직 장바구니에 해당되는 상품이 없다면 장바구니에 상품을 추가한다
+    // 2) 이미 장바구니에 해당되는 상품이 있다면, 수량만 증가시킨다
+    final exists =
+        state.firstWhereOrNull((e) => e.product.id == product.id) != null;
+
+    if (exists) {
+      state = state
+          .map(
+            (e) =>
+                e.product.id == product.id ? e.copyWith(count: e.count + 1) : e,
+          )
+          .toList();
+    } else {
+      state = [...state, BasketItemModel(product: product, count: 1)];
+    }
+  }
+
+  Future<void> removeFromBasket({
+    required ProductModel product,
+    // 카운트와 상관없이 아예 삭제
+    bool isDelete = false,
+  }) async {
+    // 1) 장바구니에 상품이 존재할 때 (1 - 상품의 카운트가 1보다 크면 -1, 아니면 장바구니에서 상품 제거)
+    // 2) 장바구니에 상품이 존재하지 않을 때 즉시 함수를 반환하고 아무것도 하지 않는다.
+    final exists =
+        state.firstWhereOrNull((e) => e.product.id == product.id) != null;
+
+    if (!exists) {
+      return;
+    }
+
+    final existingProduct = state.firstWhere((e) => e.product.id == product.id);
+
+    if (existingProduct.count == 1 || isDelete) {
+      state = state.where((e) => e.product.id != product.id).toList();
+    } else {
+      state = state
+          .map(
+            (e) =>
+                e.product.id == product.id ? e.copyWith(count: e.count - 1) : e,
+          )
+          .toList();
+    }
+  }
+}
--- lib/features/user/repository/user_repository.dart
+++ lib/features/user/repository/user_repository.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart' hide Headers;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutterskills/common/const/data.dart';
 import 'package:flutterskills/common/dio/dio_interceptor.dart';
+import 'package:flutterskills/features/user/model/basket_item_model.dart';
+import 'package:flutterskills/features/user/model/patch_basket_body.dart';
 import 'package:flutterskills/features/user/model/user_model.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
@@ -16,6 +18,16 @@ abstract class UserRepository {
   @GET('/')
   @Headers({'accessToken': 'true'})
   Future<UserModel> getMe();
+
+  @GET('/basket')
+  @Headers({'accessToken': 'true'})
+  Future<List<BasketItemModel>> getBasket();
+
+  @PATCH('/basket')
+  @Headers({'accessToken': 'true'})
+  Future<List<BasketItemModel>> patchBasket({
+    @Body() required PatchBasketBody body,
+  });
 }
 
 final userRepositoryProvider = Provider<UserRepository>((ref) {
